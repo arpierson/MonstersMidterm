3b91b3e100cb7afcbcd22801c44641dece588d71
Allen Pierson <apierso2@my.westga.edu>
2019-02-13 18:20:59 -0500

    - Initial commit. - Created all packages. - Created Participant superclass and necessary methods.

---
+++ src/edu/westga/cs6312/monsters/model/Participant.java
@@ -0,0 +1,37 @@
+package edu.westga.cs6312.monsters.model;
+
+/**
+ * Abstract class that defines a game participant.
+ * @author Allen Pierson
+ *@version 02132019
+ */
+public abstract class Participant {
+	private int healthCredits;
+	
+	/**
+	 * A 1-parameter constructor that instantiates the instance variable.
+	 * @param healthCredits	number of health credits a participant has
+	 */
+	public Participant(int healthCredits) {
+		this.healthCredits = healthCredits;
+	}
+	
+	/**
+	 * Getter method for the Participant's current number of health credits
+	 * @return	number of current health credits
+	 */
+	public int getHealthCredits() {
+		return this.healthCredits;
+	}
+
+	@Override
+	public String toString() {
+		return this.healthCredits + " remaining.";
+	}
+	
+	/**
+	 * Abstract method that defines fighting in the game.
+	 * @return	the number of health credits to be deducted from the participant being fought.
+	 */
+	public abstract int fight();
+}
