23eca85be9dc0d5a524c76d9600a49e628b36af5
Allen Pierson <apierso2@my.westga.edu>
2019-02-13 18:41:37 -0500

    - Created the KickingMonster class with toString() and fight() overrides.

---
+++ src/edu/westga/cs6312/monsters/model/KickingMonster.java
@@ -0,0 +1,27 @@
+package edu.westga.cs6312.monsters.model;
+
+/**
+ * Child class of Participant that defines a Kicking Monster in the game.
+ * @author Allen Pierson
+ * @version 02132019
+ */
+public class KickingMonster extends Participant {
+	
+	/**
+	 * 0-parameter constructor that instantiates a KickingMonster object.
+	 */
+	public KickingMonster() {
+		super(100);
+	}
+
+	@Override
+	public String toString() {
+		return "A Kicking Monster with " + super.toString();
+	}
+
+	@Override
+	public int fight() {
+		return 30;
+	}
+
+}
