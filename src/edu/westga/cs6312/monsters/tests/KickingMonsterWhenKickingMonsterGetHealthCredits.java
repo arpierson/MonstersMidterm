427f3903630be6251ba23b58474046c7c692c35a
Allen Pierson <apierso2@my.westga.edu>
2019-02-20 00:18:09 -0500

    - Added more JUnit tests for KickingMonster.getHealthCredits and .setHealthCredits

--- src/edu/westga/cs6312/monsters/tests/KickingMonsterWhenKickingMonsterGetHealthCredits.java
+++ src/edu/westga/cs6312/monsters/tests/KickingMonsterWhenKickingMonsterGetHealthCredits.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import edu.westga.cs6312.monsters.model.KickingMonster;
+import edu.westga.cs6312.monsters.model.Player;
 
 /**
  * JUnit test class for KickingMonster getHealthCredits()
@@ -14,10 +15,19 @@ import edu.westga.cs6312.monsters.model.KickingMonster;
 class KickingMonsterWhenKickingMonsterGetHealthCredits {
 
 	@Test
-	void testKickingMonsterGetHealthCredits() {
+	void testNewKickingMonsterGetHealthCredits() {
 		KickingMonster testMonster = new KickingMonster();
 		int result = testMonster.getHealthCredits();
 		assertEquals(100, result);
+	}
+	
+	@Test
+	void testKickingMonsterGetHealthCreditsAfterSimulatedFight() {
+		KickingMonster testMonster = new KickingMonster();
+		Player testPlayer = new Player();
+		testMonster.setHealthCredits(testPlayer.fight());
+		int result = testMonster.getHealthCredits();
+		assertEquals(50, result);
 	}
 
 }
